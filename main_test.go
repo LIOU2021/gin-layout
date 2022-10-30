48258e1bd4a1de26b01479ff709d3027888ed50c
LIOU <aa78789898tw@gmail.com>
2022-10-30 14:05:46 +0800

    refactory test

--- main_test.go
+++ main_test.go
@@ -6,20 +6,29 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
-func setupRouter(method string, url string, body io.Reader) *httptest.ResponseRecorder {
-	router := GetRouter()
+var (
+	router *gin.Engine
+	w      *httptest.ResponseRecorder
+)
 
-	w := httptest.NewRecorder()
+func init() {
+	router = GetRouter()
+
+	w = httptest.NewRecorder()
+}
+
+func api(method string, url string, body io.Reader) *httptest.ResponseRecorder {
 	req, _ := http.NewRequest(method, url, body)
 	router.ServeHTTP(w, req)
 	return w
 }
 
 func Test_Welcome(t *testing.T) {
-	w := setupRouter("GET", "/", nil)
+	w := api("GET", "/", nil)
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Contains(t, w.Body.String(), "welcome !")
