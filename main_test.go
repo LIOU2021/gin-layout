f4fabdf109379059df202373bdaa7e2356a49842
LIOU <aa78789898tw@gmail.com>
2022-10-30 13:40:10 +0800

    modified test structor

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,12 +9,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Router(t *testing.T) {
+func setupRouter(method string, url string, body io.Reader) *httptest.ResponseRecorder {
 	router := GetRouter()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest(method, url, body)
 	router.ServeHTTP(w, req)
+	return w
+}
+
+func Test_Router(t *testing.T) {
+	w := setupRouter("GET", "/", nil)
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Contains(t, w.Body.String(), "welcome !")
