0e3cce066c8a8fe5c72d37d8b2470fe9c0ed03ef
LIOU <aa78789898tw@gmail.com>
2022-11-06 11:28:55 +0000

    fix(orm):insert data

--- model/user.go
+++ model/user.go
@@ -6,9 +6,9 @@ import (
 
 type User struct {
 	//gorm為model的tag標籤，v2版的auto_increment要放在type裡面，v1版是放獨立定義
-	ID         int64     `gorm:"type:bigint(20) NOT NULL auto_increment;primary_key;" json:"id,omitempty"`
-	UserName   string    `gorm:"type:varchar(20) NOT NULL;" json:"username,omitempty"`
-	Password   string    `gorm:"type:varchar(100) NOT NULL;" json:"password,omitempty"`
-	Created_at time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP" json:"created_at,omitempty"`
-	Update_at  time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP" json:"updated_at,omitempty"`
+	ID        int64      `gorm:"type:bigint(20) NOT NULL auto_increment;primary_key;" json:"id,omitempty"`
+	UserName  string     `gorm:"type:varchar(20) NOT NULL;" json:"username,omitempty"`
+	Password  string     `gorm:"type:varchar(100) NOT NULL;" json:"password,omitempty"`
+	CreatedAt *time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP" json:"created_at,omitempty"`
+	UpdatedAt *time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP" json:"updated_at,omitempty"`
 }
