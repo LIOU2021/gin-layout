663d5fbfd0dd96d7612f6c750cc24e44d9d74487
LIOU <aa78789898tw@gmail.com>
2022-11-06 07:43:03 +0000

    feat(DB):mysql conn

--- core/app.go
+++ core/app.go
@@ -2,15 +2,40 @@ package core
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/LIOU2021/gin-layout/env"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var endPoint string
+
+var db *gorm.DB
+
+func initDB() {
+	// 組合sql連線字串
+	addr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True", env.DbSetting.UserName, env.DbSetting.Password, env.DbSetting.Addr, env.DbSetting.Port, env.DbSetting.Database)
+	// 連接MySQL
+	conn, err := gorm.Open(mysql.Open(addr), &gorm.Config{})
+	if err != nil {
+		log.Fatal("connection to mysql failed:", err)
+		return
+	}
+	// 設定ConnMaxLifetime/MaxIdleConns/MaxOpenConns
+	db, err1 := conn.DB()
+	if err1 != nil {
+		log.Fatal("get db failed:", err)
+		return
+	}
+	db.SetConnMaxLifetime(time.Duration(env.DbSetting.MaxLifetime) * time.Second)
+	db.SetMaxIdleConns(env.DbSetting.MaxIdleConns)
+	db.SetMaxOpenConns(env.DbSetting.MaxOpenConns)
+}
 
 func iniEnv() {
 	endPoint = env.AppSetting.HOST + ":" + env.AppSetting.PORT
@@ -50,6 +75,6 @@ func ListenAndServe(router *gin.Engine) {
 	}
 
 	appInfo()
-
+	initDB()
 	s.ListenAndServe()
 }
